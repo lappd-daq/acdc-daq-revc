14e5d8512970e48e4edcdf9f7e826b784d830f05
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-01-18 14:30:57 +0100

    Update calibratePed.cpp

--- src/calibratePed.cpp
+++ src/calibratePed.cpp
@@ -30,7 +30,7 @@ using namespace std;
 #define NUM_CH_PER_PSEC 6 
 #define NUM_BOARDS 8
 #define NUM_SAMPLE 256
-#define N_EVENTS 100
+#define N_EVENTS 1000
 std::atomic<bool> quit(false); //signal flag
 
 void got_signal(int)
@@ -62,15 +62,18 @@ int main()
 		cout << "Initialization failed!" << endl;
 		return 0;
 	}
-
+	acc.emptyUsbLine();
+	acc.dumpData();
+	
 	for(int i=0; i<N_EVENTS; i++){
 		acc.softwareTrigger();
 
-		acc.listenForAcdcData(trigMode, raw, "Config");
+		retval = acc.listenForAcdcData(trigMode, raw, "Config");
 		if (retval!=0)
 		{
 			cout << "retval " << retval << endl;
 			i--;
+			acc.dumpData();
 			continue;
 		}
 	}
