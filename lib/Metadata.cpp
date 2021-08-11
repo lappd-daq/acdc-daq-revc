8f786a38a2ff2e5974e4dc51cf563e9649e1c370
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-08-11 02:46:46 -0700

    Update Metadata.cpp

--- lib/Metadata.cpp
+++ lib/Metadata.cpp
@@ -162,17 +162,7 @@ int Metadata::parseBuffer(vector<unsigned short> buffer, unsigned short bi)
 		meta.push_back((0xDCB0 | CHIP));
 		for(int INFOWORD=0; INFOWORD<13; INFOWORD++)
 		{
-			if(CHIP==4 && INFOWORD==7)
-			{
-				meta.push_back(((PsecInfo[CHIP][INFOWORD] & 0xf000)>>12));
-				meta.push_back(((PsecInfo[CHIP][INFOWORD] & 0x800)>>11));
-				meta.push_back(((PsecInfo[CHIP][INFOWORD] & 0x400)>>10));
-				meta.push_back((PsecInfo[CHIP][INFOWORD] & 0x3ff));
-			}else
-			{
-				meta.push_back(PsecInfo[CHIP][INFOWORD]);
-			}
-			
+			meta.push_back(PsecInfo[CHIP][INFOWORD]);		
 		}
 		for(int TRIGGERWORD=0; TRIGGERWORD<6; TRIGGERWORD++)
 		{
