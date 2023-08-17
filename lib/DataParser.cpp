0d91c56a2a1f960a792cb8df529a87c4edbb7656
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-08-17 16:54:51 +0200

    v0.9.2.2

--- lib/DataParser.cpp
+++ lib/DataParser.cpp
@@ -141,7 +141,7 @@ void DataParser::writeErrorLog(string errorMsg)
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
     std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%m-%d-%Y %X");
+    //ss << std::put_time(std::localtime(&in_time_t), "%m-%d-%Y %X");
     os_err << "------------------------------------------------------------" << endl;
     os_err << ss.str() << endl;
     os_err << errorMsg << endl;
