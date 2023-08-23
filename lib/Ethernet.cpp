c4a7dedb679c161cca0ca2902980d28329380601
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-08-23 17:41:24 +0200

    dbg

--- lib/Ethernet.cpp
+++ lib/Ethernet.cpp
@@ -209,7 +209,7 @@ std::vector<uint64_t> Ethernet::RecieveBurst(int numwords, int timeout_sec, int 
     uint64_t functionreturn = 0xeeeebb01;
     bool firstread = true;
     
-    numwords = numwords + 4;
+    numwords = numwords + 10;
 
     std::vector<uint64_t> data(numwords);
 
