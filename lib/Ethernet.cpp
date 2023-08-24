96ace328ba29b94b7175cc667e0ff280924eef7b
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-08-24 17:05:56 +0200

    v0.9.8.1

--- lib/Ethernet.cpp
+++ lib/Ethernet.cpp
@@ -235,7 +235,7 @@ std::vector<uint64_t> Ethernet::RecieveBurst(int numwords, int timeout_sec, int 
         int retval = select(m_socket+1, &rfds_, NULL, NULL, &tv_);
         if(retval > 0)
         {
-            if((numbytes = recvfrom(m_socket,buffer,how_much_to_read,0,(struct sockaddr*)&their_addr,&addr_len)) == -1)
+            if((numbytes = recvfrom(m_socket,buffer,2*how_much_to_read,0,(struct sockaddr*)&their_addr,&addr_len)) == -1)
             {
                 perror("recvfrom");
                 functionreturn = 0xeeeebb02;
