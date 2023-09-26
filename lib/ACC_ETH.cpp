10bac0c85bea7978f85494967035378ff634fbf9
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-09-26 16:45:56 +0200

    v0.9.9.32

--- lib/ACC_ETH.cpp
+++ lib/ACC_ETH.cpp
@@ -739,13 +739,13 @@ vector<uint64_t> ACC_ETH::Temp_Read(int trigMode, vector<int> LAPPD_on_ACC)
 
 		if(chrono::duration_cast<chrono::milliseconds>(now - start) > timeoutDuration)
 		{
-			return -601;
+			return {-601};
 		}
 
 		//If sigint happens, return value of 3
 		if(quitacc.load())
 		{
-			return -602;
+			return {-602};
 		}
 
         //Determine buffers and create info frame
