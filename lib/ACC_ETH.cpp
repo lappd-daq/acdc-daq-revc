ce96963d324d86b89fe174c0f5f9f561a6f4fae7
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-03-13 15:39:00 +0100

    modified:   lib/ACC_ETH.cpp

--- lib/ACC_ETH.cpp
+++ lib/ACC_ETH.cpp
@@ -555,6 +555,7 @@ void ACC_ETH::VersionCheck()
             std::cout << "ACDC boards " << bi << " was not detected" << endl;
         }
     }
+    eth->SendData(CML_ACC.RX_Buffer_Reset_Request,(1<<bi),"w");
 }
 
 //------------------------------------------------------------------------------------//
